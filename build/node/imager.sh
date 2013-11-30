f23f9034889526782a971c5ae8591222170548f7
Luke Young <innoying@ubuntu.(none)>
2013-11-30 10:14:17 -0800

    Fixed imager

--- build/node/imager.sh
+++ build/node/imager.sh
@@ -4,7 +4,7 @@ log "node: Running node installer"
 log "node: Starting SquashFS Edit"
 ROOTFS="$(begin_squashfs_edit 'rootfs')"
 log "node: SquashFS Mounted"
-cp -R destdir/* /
+cp -R "destdir/*" "${ROOTFS}/"
 log "node: SquashFS Finished"
 end_squashfs_edit "$ROOTFS"
 log "node: Done!"
